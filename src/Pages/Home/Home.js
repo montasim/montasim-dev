6779796d031056f8fa3978fae47e7f4341a7070c
Ｍ♢ＮＴΛＳＩＭ <montasimmamun@gmail.com>
2022-06-09 01:25:13 +0600

    my projects section

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import AboutMe from './AboutMe';
 import AboutMeBanner from './AboutMeBanner';
+import MyProjects from './MyProjects';
 
 const Home = () => {
     return (
         <div>
             <AboutMeBanner />
             <AboutMe />
+            <MyProjects />
         </div>
     );
 };
---
+++ src/Pages/Home/MyProjects.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const MyProjects = () => {
+    return (
+        <div class="hero bg-base-200 pt-20">
+            <div class="hero-content text-center">
+                <div class="max-w-md">
+                    <h1 class="text-4xl font-bold uppercase">My Projects</h1>
+
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default MyProjects;
