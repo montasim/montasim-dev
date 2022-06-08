a6d5d35cf8a3f34368e3832f6936d028b7e882fb
Ｍ♢ＮＴΛＳＩＭ <montasimmamun@gmail.com>
2022-06-08 22:50:04 +0600

    footer

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import Footer from "./Components/Footer";
 import Navbar from "./Components/Navbar";
 
 function App() {
   return (
     <div>
-      <Navbar />
+      <Navbar />'
+      <Footer />
     </div>
   );
 }
---
+++ src/Components/Footer.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { BsLinkedin, BsGithub, BsFacebook } from 'react-icons/bs';
+
+const Footer = () => {
+    return (
+        <footer class="footer footer-center p-10 bg-base-200 text-base-content rounded">
+            <div class="grid grid-flow-col gap-4 text-lg">
+                <a>Home</a>
+                <a>About Me</a>
+                <a>Portfolio</a>
+                <a>Contact</a>
+                <a>Blog</a>
+                <a>Resume</a>
+            </div>
+            <div>
+                <div class="grid grid-flow-col gap-4 text-3xl">
+                    <a><BsLinkedin /></a>
+                    <a><BsGithub /></a>
+                    <a><BsFacebook /></a>
+                </div>
+            </div>
+            <div>
+                <p>This portfolio is made by React with
+                    ❤️</p>
+            </div>
+        </footer>
+    );
+};
+
+export default Footer;
