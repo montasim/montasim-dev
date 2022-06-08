e2afadb6d863b8814a8ad92d8c4a10bf882e53db
Ｍ♢ＮＴΛＳＩＭ <montasimmamun@gmail.com>
2022-06-09 01:36:56 +0600

    my service section

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -2,12 +2,14 @@ import React from 'react';
 import AboutMe from './AboutMe';
 import AboutMeBanner from './AboutMeBanner';
 import MyProjects from './MyProjects';
+import MyService from './MyService';
 
 const Home = () => {
     return (
         <div>
             <AboutMeBanner />
             <AboutMe />
+            <MyService />
             <MyProjects />
         </div>
     );
