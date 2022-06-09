f1c76d5432e4fea0211bb3196dcf34989f7709c2
Ｍ♢ＮＴΛＳＩＭ <montasimmamun@gmail.com>
2022-06-09 09:24:52 +0600

    react router installed

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import Footer from "./Components/Footer";
 import Navbar from "./Components/Navbar";
 import Home from "./Pages/Home/Home";
+import { Routes, Route } from "react-router-dom";
+import NotFound from "./Pages/NotFound/NotFound";
 
 function App() {
   return (
     <div className="bg-gray-900">
       <Navbar />
-      <Home />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
       <Footer />
     </div>
   );
