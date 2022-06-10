53b297768daf009f66ad655ee714c99ed451e34e
Ｍ♢ＮＴΛＳＩＭ <montasimmamun@gmail.com>
2022-06-10 12:33:01 +0600

    navbar padding

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -11,7 +11,7 @@ const Navbar = () => {
     </>;
 
     return (
-        <div class="navbar bg-gray-900 uppercase">
+        <div class="navbar bg-gray-900 uppercase p-8">
             <div class="navbar-start">
                 <div class="dropdown">
                     <label tabindex="0" class="btn btn-ghost lg:hidden">
