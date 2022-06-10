005c05f8c27c432f1d8f5add269113603edaaec4
Ｍ♢ＮＴΛＳＩＭ <montasimmamun@gmail.com>
2022-06-10 21:15:07 +0600

    my skills

--- src/Pages/Home/MyService.js
+++ src/Pages/Home/MyService.js
@@ -1,209 +1,129 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { BiDesktop } from 'react-icons/bi';
+import { FaReact, FaNodeJs } from 'react-icons/fa';
+import { MdSecurity } from 'react-icons/md';
+import { SiMongodb, SiHackthebox } from 'react-icons/si';
 
 const MyService = () => {
     return (
-        <section data-aos="fade-zoom-in"
-            data-aos-easing="ease-in-back"
-            data-aos-delay="300"
-            data-aos-offset="0" id='services' class="text-white bg-gray-900">
-            <div class="max-w-screen-xl px-4 py-16 mx-auto sm:px-6 lg:px-8">
-                <div class="max-w-lg mx-auto text-center">
-                    <h2 class="text-3xl font-bold sm:text-4xl uppercase">My services</h2>
+        <aside data-aos='fade-down'
+            data-aos-easing='linear'
+            data-aos-duration='1500' className='text-white bg-gray-900 pt-20'>
+            <div className='max-w-screen-xl px-4 py-16 mx-auto sm:px-6 lg:px-8'>
+                <div className='w-3/6 mx-auto text-center'>
+                    <h2 className='text-3xl font-bold sm:text-4xl'>My Specialty in Web Development</h2>
 
-                    <p class="mt-4 text-gray-300 uppercase">
-                        - What I Provide -
+                    <p className='mt-4 text-gray-300'>
+                        I am a computer science professionals with advance knowledge of MERN stack web development. I love problem solving and explore new technologies.
                     </p>
                 </div>
 
-                <div class="grid grid-cols-1 gap-8 mt-8 md:grid-cols-2 lg:grid-cols-3">
-                    <a
-                        class="block p-8 transition border border-gray-800 shadow-xl rounded-xl hover:shadow-primary/10 hover:border-primary/10"
-                        href="/services/digital-campaigns"
+                <div className='grid grid-cols-1 gap-8 mt-8 md:grid-cols-2 lg:grid-cols-3'>
+                    <Link
+                        className='block p-8 transition border border-gray-800 shadow-xl rounded-xl hover:shadow-primary/10 hover:border-primary/10'
+                        to='/services/digital-campaigns'
                     >
+                        <BiDesktop className='text-primary text-4xl' />
+
+                        <h3 className='mt-4 text-xl font-bold text-white'>UI Design</h3>
+
+                        <p className='mt-1 text-sm text-gray-300'>
+                            Modern and clean looking UI.
+                        </p>
+                    </Link>
+
+                    <Link
+                        className='block p-8 transition border border-gray-800 shadow-xl rounded-xl hover:shadow-primary/10 hover:border-primary/10'
+                        to='/services/digital-campaigns'
+                    >
+                        <SiHackthebox className='text-primary text-4xl' />
+
+                        <h3 className='mt-4 text-xl font-bold text-white'>UX Design</h3>
+
+                        <p className='mt-1 text-sm text-gray-300'>
+                            Smooth user experience.
+                        </p>
+                    </Link>
+
+                    <Link
+                        className='block p-8 transition border border-gray-800 shadow-xl rounded-xl hover:shadow-primary/10 hover:border-primary/10'
+                        to='/services/digital-campaigns'
+                    >
+                        <SiMongodb className='text-primary text-4xl' />
+                        <h3 className='mt-4 text-xl font-bold text-white'>MongoDB Database</h3>
+
+                        <p className='mt-1 text-sm text-gray-300'>
+                            Modern and secure database.
+                        </p>
+                    </Link>
+
+                    <Link
+                        className='block p-8 transition border border-gray-800 shadow-xl rounded-xl hover:shadow-primary/10 hover:border-primary/10'
+                        to='/services/digital-campaigns'
+                    >
+                        <FaNodeJs className='text-primary text-4xl' />
+
+                        <h3 className='mt-4 text-xl font-bold text-white'>NodeJs Backend</h3>
+
+                        <p className='mt-1 text-sm text-gray-300'>
+                            Advance backend server.
+                        </p>
+                    </Link>
+
+                    <Link
+                        className='block p-8 transition border border-gray-800 shadow-xl rounded-xl hover:shadow-primary/10 hover:border-primary/10'
+                        to='/services/digital-campaigns'
+                    >
+                        <FaReact className='text-primary text-4xl' />
+
+                        <h3 className='mt-4 text-xl font-bold text-white'>Popular frontend framework </h3>
+
+                        <p className='mt-1 text-sm text-gray-300'>
+                            World's most popular front end framework
+                        </p>
+                    </Link>
+
+                    <Link
+                        className='block p-8 transition border border-gray-800 shadow-xl rounded-xl hover:shadow-primary/10 hover:border-primary/10'
+                        to='/services/digital-campaigns'
+                    >
+                        <MdSecurity className='text-primary text-4xl' />
+
+                        <h3 className='mt-4 text-xl font-bold text-white'>World class security</h3>
+
+                        <p className='mt-1 text-sm text-gray-300'>
+                            Ensure security with JWT.
+                        </p>
+                    </Link>
+                </div>
+
+                <div className='mt-12 text-center'>
+                    <a
+                        className='inline-flex items-center px-8 py-3 mt-8 text-white bg-gradient-to-r from-primary to-primary'
+                        href='https://www.linkedin.com/in/montasim'
+                        target='_blank' rel="noreferrer"
+                    >
+                        <span className='text-sm font-medium'> Hire Me </span>
+
                         <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            class="w-10 h-10 text-primary"
-                            fill="none"
-                            viewBox="0 0 24 24"
-                            stroke="currentColor"
+                            className='w-5 h-5 ml-3'
+                            xmlns='http://www.w3.org/2000/svg'
+                            fill='none'
+                            viewBox='0 0 24 24'
+                            stroke='currentColor'
                         >
-                            <path d="M12 14l9-5-9-5-9 5 9 5z" />
                             <path
-                                d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z"
-                            />
-                            <path
-                                stroke-linecap="round"
-                                stroke-linejoin="round"
-                                stroke-width="2"
-                                d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222"
+                                strokeLinecap='round'
+                                strokeLinejoin='round'
+                                strokeWidth='2'
+                                d='M17 8l4 4m0 0l-4 4m4-4H3'
                             />
                         </svg>
-
-                        <h3 class="mt-4 text-xl font-bold text-white">Full Stack Website</h3>
-
-                        <p class="mt-1 text-sm text-gray-300">
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex ut quo
-                            possimus adipisci distinctio alias voluptatum blanditiis laudantium.
-                        </p>
-                    </a>
-
-                    <a
-                        class="block p-8 transition border border-gray-800 shadow-xl rounded-xl hover:shadow-primary/10 hover:border-primary/10"
-                        href="/services/digital-campaigns"
-                    >
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            class="w-10 h-10 text-primary"
-                            fill="none"
-                            viewBox="0 0 24 24"
-                            stroke="currentColor"
-                        >
-                            <path d="M12 14l9-5-9-5-9 5 9 5z" />
-                            <path
-                                d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z"
-                            />
-                            <path
-                                stroke-linecap="round"
-                                stroke-linejoin="round"
-                                stroke-width="2"
-                                d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222"
-                            />
-                        </svg>
-
-                        <h3 class="mt-4 text-xl font-bold text-white">Front-end Website</h3>
-
-                        <p class="mt-1 text-sm text-gray-300">
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex ut quo
-                            possimus adipisci distinctio alias voluptatum blanditiis laudantium.
-                        </p>
-                    </a>
-
-                    <a
-                        class="block p-8 transition border border-gray-800 shadow-xl rounded-xl hover:shadow-primary/10 hover:border-primary/10"
-                        href="/services/digital-campaigns"
-                    >
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            class="w-10 h-10 text-primary"
-                            fill="none"
-                            viewBox="0 0 24 24"
-                            stroke="currentColor"
-                        >
-                            <path d="M12 14l9-5-9-5-9 5 9 5z" />
-                            <path
-                                d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z"
-                            />
-                            <path
-                                stroke-linecap="round"
-                                stroke-linejoin="round"
-                                stroke-width="2"
-                                d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222"
-                            />
-                        </svg>
-
-                        <h3 class="mt-4 text-xl font-bold text-white">Website Design</h3>
-
-                        <p class="mt-1 text-sm text-gray-300">
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex ut quo
-                            possimus adipisci distinctio alias voluptatum blanditiis laudantium.
-                        </p>
-                    </a>
-
-                    <a
-                        class="block p-8 transition border border-gray-800 shadow-xl rounded-xl hover:shadow-primary/10 hover:border-primary/10"
-                        href="/services/digital-campaigns"
-                    >
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            class="w-10 h-10 text-primary"
-                            fill="none"
-                            viewBox="0 0 24 24"
-                            stroke="currentColor"
-                        >
-                            <path d="M12 14l9-5-9-5-9 5 9 5z" />
-                            <path
-                                d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z"
-                            />
-                            <path
-                                stroke-linecap="round"
-                                stroke-linejoin="round"
-                                stroke-width="2"
-                                d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222"
-                            />
-                        </svg>
-
-                        <h3 class="mt-4 text-xl font-bold text-white">Web App Development</h3>
-
-                        <p class="mt-1 text-sm text-gray-300">
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex ut quo
-                            possimus adipisci distinctio alias voluptatum blanditiis laudantium.
-                        </p>
-                    </a>
-
-                    <a
-                        class="block p-8 transition border border-gray-800 shadow-xl rounded-xl hover:shadow-primary/10 hover:border-primary/10"
-                        href="/services/digital-campaigns"
-                    >
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            class="w-10 h-10 text-primary"
-                            fill="none"
-                            viewBox="0 0 24 24"
-                            stroke="currentColor"
-                        >
-                            <path d="M12 14l9-5-9-5-9 5 9 5z" />
-                            <path
-                                d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z"
-                            />
-                            <path
-                                stroke-linecap="round"
-                                stroke-linejoin="round"
-                                stroke-width="2"
-                                d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222"
-                            />
-                        </svg>
-
-                        <h3 class="mt-4 text-xl font-bold text-white">Website Re-Design</h3>
-
-                        <p class="mt-1 text-sm text-gray-300">
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex ut quo
-                            possimus adipisci distinctio alias voluptatum blanditiis laudantium.
-                        </p>
-                    </a>
-
-                    <a
-                        class="block p-8 transition border border-gray-800 shadow-xl rounded-xl hover:shadow-primary/10 hover:border-primary/10"
-                        href="/services/digital-campaigns"
-                    >
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            class="w-10 h-10 text-primary"
-                            fill="none"
-                            viewBox="0 0 24 24"
-                            stroke="currentColor"
-                        >
-                            <path d="M12 14l9-5-9-5-9 5 9 5z" />
-                            <path
-                                d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z"
-                            />
-                            <path
-                                stroke-linecap="round"
-                                stroke-linejoin="round"
-                                stroke-width="2"
-                                d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222"
-                            />
-                        </svg>
-
-                        <h3 class="mt-4 text-xl font-bold text-white">Website Bug Fixing</h3>
-
-                        <p class="mt-1 text-sm text-gray-300">
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex ut quo
-                            possimus adipisci distinctio alias voluptatum blanditiis laudantium.
-                        </p>
                     </a>
                 </div>
             </div>
-        </section>
+        </aside>
     );
 };
 
