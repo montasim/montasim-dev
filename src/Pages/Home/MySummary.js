ab776d9f07cca92385452037ab91d85a8c96ce25
Ｍ♢ＮＴΛＳＩＭ <montasimmamun@gmail.com>
2022-06-10 21:17:58 +0600

    my summary

--- src/Pages/Home/MySummary.js
+++ src/Pages/Home/MySummary.js
@@ -2,33 +2,28 @@ import React from 'react';
 
 const MySummary = () => {
     return (
-        <section data-aos="fade-down"
-            data-aos-easing="linear"
-            data-aos-duration="1500" class="text-center">
-            <div class="max-w-screen-xl px-4 py-16 mx-auto sm:px-6 lg:px-8">
-                <ul class="grid grid-cols-2 gap-4 border-2 border-primary/40 rounded-xl lg:grid-cols-4">
-                    <li class="p-8">
-                        <p class="text-2xl font-extrabold text-primary">5+</p>
-                        <p class="mt-1 text-lg font-medium">Full-stack Projects</p>
+        <aside className='text-white bg-gray-900 pb-14'>
+            <div className='max-w-screen-xl px-4 py-16 mx-auto sm:px-6 lg:px-8'>
+                <ul
+                    className='mt-8 border-2 border-white border-opacity-25 divide-y-2 divide-white sm:grid sm:divide-y-0 sm:divide-x-2 sm:border-l-0 sm:border-r-0 divide-opacity-25 sm:grid-cols-3 text-center'
+                >
+                    <li className='p-8'>
+                        <p className='text-3xl font-extrabold text-primary'>4+</p>
+                        <p className='mt-1 text-xl font-medium'>Frontend Projects</p>
                     </li>
 
-                    <li class="p-8">
-                        <p class="text-2xl font-extrabold text-primary">3+</p>
-                        <p class="mt-1 text-lg font-medium">Front-end Projects</p>
+                    <li className='p-8'>
+                        <p className='text-3xl font-extrabold text-primary'>5+</p>
+                        <p className='mt-1 text-xl font-medium'>Backend Projects</p>
                     </li>
 
-                    <li class="p-8">
-                        <p class="text-2xl font-extrabold text-primary">2+</p>
-                        <p class="mt-1 text-lg font-medium">Backend Projects</p>
-                    </li>
-
-                    <li class="p-8">
-                        <p class="text-2xl font-extrabold text-primary">1</p>
-                        <p class="mt-1 text-lg font-medium">Blog</p>
+                    <li className='p-8'>
+                        <p className='text-3xl font-extrabold text-primary'>3000+</p>
+                        <p className='mt-1 text-xl font-medium'>Commits This Year</p>
                     </li>
                 </ul>
             </div>
-        </section>
+        </aside>
     );
 };
 
