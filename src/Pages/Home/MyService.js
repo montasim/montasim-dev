ff866c336398fc0a365be098bcc8ff2029a1d9b0
Ｍ♢ＮＴΛＳＩＭ <montasimmamun@gmail.com>
2022-06-10 13:04:14 +0600

    get started btn removed

--- src/Pages/Home/MyService.js
+++ src/Pages/Home/MyService.js
@@ -202,30 +202,6 @@ const MyService = () => {
                         </p>
                     </a>
                 </div>
-
-                <div class="mt-12 text-center">
-                    <a
-                        class="inline-flex items-center px-8 py-3 mt-8 text-white bg-primary border border-primary rounded hover:bg-transparent active:text-primary focus:outline-none focus:ring"
-                        href="/get-started"
-                    >
-                        <span class="text-sm font-medium"> Get Started </span>
-
-                        <svg
-                            class="w-5 h-5 ml-3"
-                            xmlns="http://www.w3.org/2000/svg"
-                            fill="none"
-                            viewBox="0 0 24 24"
-                            stroke="currentColor"
-                        >
-                            <path
-                                stroke-linecap="round"
-                                stroke-linejoin="round"
-                                stroke-width="2"
-                                d="M17 8l4 4m0 0l-4 4m4-4H3"
-                            />
-                        </svg>
-                    </a>
-                </div>
             </div>
         </section>
     );
