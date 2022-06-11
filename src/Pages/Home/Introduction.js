2b08f0e59644b1be9091ba781ab10953f8b7baee
Ｍ♢ＮＴΛＳＩＭ <montasimmamun@gmail.com>
2022-06-11 13:13:51 +0600

    html error

--- src/Pages/Home/Introduction.js
+++ src/Pages/Home/Introduction.js
@@ -100,7 +100,7 @@ const Introduction = () => {
                 }}
             />
             <div class="hero-content flex-col lg:flex-row-reverse">
-                <img src={professionalPhoto} class="max-w-sm rounded-lg shadow-2xl" />
+                <img src={professionalPhoto} class="max-w-sm rounded-lg shadow-2xl" alt='montasim pic' />
                 <div>
                     <p className='mb-2 text-xl'>Hello 👋, I am</p>
                     <h1 class="text-5xl font-bold uppercase">Montasim Al Mamun</h1>
@@ -111,7 +111,7 @@ const Introduction = () => {
                         <a href='https://www.facebook.com/montasimmamun/' target='_blank' rel='noreferrer'><BsFacebook /></a>
                     </div>
                     <p class="py-6 lg:w-8/12 text-[20px]">I am a proficient MERN stack developer who is passionate about developing websites and web-based applications.</p>
-                    <a class="btn btn-primary" href='https://drive.google.com/file/d/1sR4CxyIYw1uIesJHWaERKDGOBwfOVUcH/view?usp=sharing' target='_blank'>Download My Resume</a>
+                    <a class="btn btn-primary" href='https://drive.google.com/file/d/1sR4CxyIYw1uIesJHWaERKDGOBwfOVUcH/view?usp=sharing' target='_blank' rel="noreferrer">Download My Resume</a>
                 </div>
             </div>
         </section>
