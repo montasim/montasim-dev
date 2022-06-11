9761dcec8f89101f3d4cb8b3479ea946311943ea
Ｍ♢ＮＴΛＳＩＭ <montasimmamun@gmail.com>
2022-06-11 12:33:24 +0600

    ts particles

--- src/Pages/Home/Introduction.js
+++ src/Pages/Home/Introduction.js
@@ -1,11 +1,103 @@
 import React from 'react';
 import professionalPhoto from '../../assets/images/SHUVO Transparent BG.png';
+import Particles from "react-tsparticles";
+import { loadFull } from "tsparticles";
 
 const Introduction = () => {
+    const particlesInit = async (main) => {
+        console.log(main);
+
+        // you can initialize the tsParticles instance (main) here, adding custom shapes or presets
+        // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
+        // starting from v2 you can add only the features you need reducing the bundle size
+        await loadFull(main);
+    };
+
+    const particlesLoaded = (container) => {
+        console.log(container);
+    };
+
     return (
         <section data-aos="fade-right"
             data-aos-offset="300"
             data-aos-easing="ease-in-sine" class="hero min-h-screen bg-gray-900">
+            <Particles
+                id="tsparticles"
+                init={particlesInit}
+                loaded={particlesLoaded}
+                options={{
+                    background: {
+                        color: {
+                            value: "#111827",
+                        },
+                    },
+                    fpsLimit: 120,
+                    interactivity: {
+                        events: {
+                            onClick: {
+                                enable: true,
+                                mode: "push",
+                            },
+                            onHover: {
+                                enable: true,
+                                mode: "repulse",
+                            },
+                            resize: true,
+                        },
+                        modes: {
+                            push: {
+                                quantity: 4,
+                            },
+                            repulse: {
+                                distance: 200,
+                                duration: 0.4,
+                            },
+                        },
+                    },
+                    particles: {
+                        color: {
+                            value: "#ffffff",
+                        },
+                        links: {
+                            color: "#ffffff",
+                            distance: 150,
+                            enable: true,
+                            opacity: 0.5,
+                            width: 1,
+                        },
+                        collisions: {
+                            enable: true,
+                        },
+                        move: {
+                            direction: "none",
+                            enable: true,
+                            outModes: {
+                                default: "bounce",
+                            },
+                            random: false,
+                            speed: 6,
+                            straight: false,
+                        },
+                        number: {
+                            density: {
+                                enable: true,
+                                area: 800,
+                            },
+                            value: 80,
+                        },
+                        opacity: {
+                            value: 0.5,
+                        },
+                        shape: {
+                            type: "circle",
+                        },
+                        size: {
+                            value: { min: 1, max: 5 },
+                        },
+                    },
+                    detectRetina: true,
+                }}
+            />
             <div class="hero-content flex-col lg:flex-row-reverse">
                 <img src={professionalPhoto} class="max-w-sm rounded-lg shadow-2xl" />
                 <div>
