f606c1d84f188c509e76e23b49a8a6bc81634c0b
Ｍ♢ＮＴΛＳＩＭ <montasimmamun@gmail.com>
2022-06-11 09:24:51 +0600

    header & footer

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -4,10 +4,10 @@ import { BsLinkedin, BsGithub, BsFacebook } from 'react-icons/bs';
 const Footer = () => {
     return (
         <footer class="footer footer-center p-10 bg-gray-900 text-base-content rounded mt-32">
-            <div class="grid grid-flow-col gap-4 text-xl">
-                <a href='#about'>About Me</a>
-                <a href='#services'>Services</a>
-                <a href='#portfolio'>Portfolio</a>
+            <div class="grid grid-flow-col gap-4 text-lg uppercase">
+                <a href='#about'>About</a>
+                <a href='#services'>Skills</a>
+                <a href='#portfolio'>Projects</a>
                 <a href='#contact'>Contact</a>
                 <a href='https://a-coders-diary.web.app/' target='_blank' rel='noreferrer'>Blog</a>
             </div>
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -3,9 +3,9 @@ import { Link } from 'react-router-dom';
 
 const Navbar = () => {
     const navbarItems = <>
-        <li><a href='#about'>About Me</a></li>
-        <li><a href='#services'>Services</a></li>
-        <li><a href='#portfolio'>Portfolio</a></li>
+        <li><a href='#about'>About</a></li>
+        <li><a href='#services'>Skills</a></li>
+        <li><a href='#portfolio'>Projects</a></li>
         <li><a href='#contact'>Contact</a></li>
         <li><a className='btn btn-primary rounded-full text-white' href='https://a-coders-diary.web.app/' target='_blank' rel='noreferrer'>Blog</a></li>
     </>;
