d1d4351be087f5b23f0c6d99bdfbc1955ea79576
Ｍ♢ＮＴΛＳＩＭ <montasimmamun@gmail.com>
2022-07-03 00:18:33 +0600

    resume button color

--- src/Pages/Home/Introduction.js
+++ src/Pages/Home/Introduction.js
@@ -9,7 +9,7 @@ const Introduction = () => {
                     <h1 className="text-[66px] font-bold text-[#ccd6f6]">Montasim -Al- Mamun.</h1>
                     <h5 className='text-[66px] font-bold text-[#8892b0]'>I build things for the web.</h5>
                     <p className="mt-4 py-2 lg:w-7/12 text-[18px] text-[#8892b0]" > I’m a software engineer specializing in building (and occasionally designing) exceptional digital experiences. Currently, I’m focused on building accessible, human-centered products using MERN Stack.</p >
-                    <a className="btn rounded-xs border-primary text-primary px-10 capitalize text-md mt-14" href='https://drive.google.com/file/d/1sR4CxyIYw1uIesJHWaERKDGOBwfOVUcH/view?usp=sharing' target='_blank' rel="noreferrer" > Download My Resume</a >
+                    <a className="btn rounded-xs border-primary text-primary px-10 capitalize text-md mt-14 hover:bg-primary hover:opacity-70 hover:text-black" href='https://drive.google.com/file/d/1sR4CxyIYw1uIesJHWaERKDGOBwfOVUcH/view?usp=sharing' target='_blank' rel="noreferrer" > Download My Resume</a >
                 </div >
             </div >
         </section >
