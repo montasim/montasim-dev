f07d00faf7406e47192ca11868a99a8ae0e402bf
Ｍ♢ＮＴΛＳＩＭ <montasimmamun@gmail.com>
2022-07-03 00:02:04 +0600

    social media link added

--- src/Components/Social.js
+++ src/Components/Social.js
@@ -1,13 +1,13 @@
 import React from 'react';
-import { BsLinkedin, BsGithub, BsFacebook } from 'react-icons/bs';
+import { AiFillLinkedin, AiFillGithub, AiFillFacebook } from 'react-icons/ai';
 
 const Social = () => {
     return (
         <section className='fixed bottom-0 left-0 right-0 ml-10 mr-20 px-2'>
             <div className='flex flex-col gap-y-6 justify-center'>
-                <BsLinkedin className='text-2xl hover:text-3xl' />
-                <BsGithub className='text-2xl hover:text-3xl' />
-                <BsFacebook className='text-2xl hover:text-3xl' />
+                <a className='text-2xl hover:text-primary' href="https://www.linkedin.com/in/montasim" target='_blank' rel="noreferrer"><AiFillLinkedin /></a>
+                <a className='text-2xl hover:text-primary' href="https://github.com/montasim" target='_blank' rel="noreferrer"><AiFillGithub /></a>
+                <a className='text-2xl hover:text-primary' href="https://www.facebook.com/montasimmamun/" target='_blank' rel="noreferrer"><AiFillFacebook /></a>
                 <div className='bg-white h-20 w-0.5 ml-[10px]'></div>
             </div>
         </section>
