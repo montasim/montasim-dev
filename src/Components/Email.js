788f144e41719b5efb99f7cfd15ddfce1653d878
Ｍ♢ＮＴΛＳＩＭ <montasimmamun@gmail.com>
2022-07-03 00:19:47 +0600

    email hover text color

--- src/Components/Email.js
+++ src/Components/Email.js
@@ -4,7 +4,7 @@ const Email = () => {
     return (
         <section className='fixed bottom-0 right-0 mr-12 ml-20 px-2'>
             <div className='flex flex-col gap-y-6 justify-center'>
-                <a className='transform rotate-180 text-center' style={{ writingMode: 'vertical-rl' }} href='mailto:montasimmamun@gmail.com'>montasimmamun@gmail.com</a>
+                <a className='transform rotate-180 text-center hover:text-primary' style={{ writingMode: 'vertical-rl' }} href='mailto:montasimmamun@gmail.com'>montasimmamun@gmail.com</a>
                 <div className='bg-white h-20 w-0.5 ml-[13px]'></div>
             </div>
         </section>
