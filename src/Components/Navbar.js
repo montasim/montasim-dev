0024b0cfb1383318ffd1e9e5c8c998029be3a99b
Ｍ♢ＮＴΛＳＩＭ <montasimmamun@gmail.com>
2022-07-03 09:27:38 +0600

    navbar blur

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { HashLink as Link } from 'react-router-hash-link';
+import { BiMenuAltLeft } from 'react-icons/bi';
 
 const Navbar = () => {
     const navbarItems = <>
@@ -7,15 +8,16 @@ const Navbar = () => {
         <li><Link to='/#about'>About</Link></li>
         <li><Link to='/#projects'>Projects</Link></li>
         <li><Link to='/#contact'>Contact</Link></li>
-        <li><a className='border border-primary rounded-sm hover:bg-primary text-primary hover:opacity-70 hover:text-black' href='https://a-coders-diary.web.app/' target='_blank' rel='noreferrer'>Blog</a></li>
+        <li><a href='https://a-coders-diary.web.app/' target='_blank' rel='noreferrer'>Blog</a></li>
+        <li><a className='btn btn-sm pb-6 hover:bg-primary text-primary hover:opacity-70 hover:text-black' href='https://drive.google.com/file/d/1sR4CxyIYw1uIesJHWaERKDGOBwfOVUcH/view?usp=sharing' target='_blank' rel='noreferrer'>Resume</a></li>
     </>;
 
     return (
-        <div className="navbar sticky top-0 bg-[#112240]" >
+        <div className="navbar sticky top-0 bg-[#112240] opacity-95" >
             <div className="navbar-start" >
                 <div className="dropdown" >
                     <label tabIndex="0" className="btn btn-ghost lg:hidden" >
-                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor" > <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg >
+                        <BiMenuAltLeft className='text-4xl' />
                     </label >
                     <ul tabIndex="0" className="menu menu-compact dropdown-content mt-3 p-2 shadow rounded-box w-52 bg-[#112240]" >
                         {
@@ -23,7 +25,7 @@ const Navbar = () => {
                         }
                     </ul >
                 </div >
-                <Link className="btn btn-ghost text-xl" to='/' > Montasim -Al- Mamun</Link >
+                <Link className="btn border-0 lg:text-xl md:text-xl text-md text-primary uppercase lg:ml-10 md:ml-6 ml-0 hover:bg-[#112240]" to='/' > Montasim -Al- Mamun</Link >
             </div >
 
             <div className="navbar-center" >
@@ -31,7 +33,7 @@ const Navbar = () => {
             </div >
 
             <div className="navbar-end hidden lg:flex" >
-                <ul className="menu menu-horizontal p-0 items-center" >
+                <ul className="menu menu-horizontal p-0 items-center mr-10" >
                     {
                         navbarItems
                     }
