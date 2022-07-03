ef9014597126112d4a8e51f9eb5a5c70646b3067
Ｍ♢ＮＴΛＳＩＭ <montasimmamun@gmail.com>
2022-07-03 09:39:52 +0600

    footer

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -4,7 +4,7 @@ import { BsLinkedin, BsGithub, BsFacebook } from 'react-icons/bs';
 const Footer = () => {
     return (
         <footer className="footer footer-center mt-32 mb-10  text-base-content text-lg rounded" >
-            <p className='text-md'>This portfolio is made by React with ❤️</p>
+            <small className='flex text-md text-[#8892b0]'>Designed & Built by <a className='text-primary' href='https://github.com/montasim' target='_blank' rel='noreferrer'>Montasim -Al- Mamun</a> with ❤️</small>
         </footer >
     );
 };
