2b5c7a96814687685396c8493c06ec12738575b0
Ｍ♢ＮＴΛＳＩＭ <montasimmamun@gmail.com>
2022-07-04 10:49:09 +0600

    color code added

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,12 @@ module.exports = {
           primary: '#64ffda',
           accent: '#ccd6f6',
           'base-100': '#0a192f',
+          'tertiary': '#112240',
+          'quaternary': '#233554',
+          'quinary': '#e6f1ff',
+          'senary': '#ccd6f6',
+          'septenary': '#a8b2d1',
+          'octonary': '#8892b0',
         },
       }
     ],
